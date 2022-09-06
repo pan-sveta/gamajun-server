688bd61525b33409505f9aa6a5ff2d7a24f86040
Filip Štěpánek <stepafil@gmail.com>
2022-09-06 21:31:55 +0200

    Empty project

---
+++ src/main/java/app/stepanek/gamajun/GamajunApplication.java
@@ -0,0 +1,13 @@
+package app.stepanek.gamajun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamajunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamajunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/stepanek/gamajun/GamajunApplicationTests.java
@@ -0,0 +1,13 @@
+package app.stepanek.gamajun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamajunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
